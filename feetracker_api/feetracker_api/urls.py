371538082ed2a1a9c9bd358778a064c0ac5e0c13
awen <alduanecuevasmirasol@gmail.com>
2025-09-26 15:14:02 +0800

    updated

--- feetracker_api/feetracker_api/urls.py
+++ feetracker_api/feetracker_api/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('feetracker/', include('app.urls')),
+    path('api/', include('app.urls')),
 ]
